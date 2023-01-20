6d3b863ac8ab827f763733a7079ece3efa2c936d
Pureun <poornnara2@gmail.com>
2023-01-20 17:06:41 +0900

    어제풀던 문제아직도전중

--- app/src/main/java/algorithm/study/pureun/programmers/Prg_120888.java
+++ app/src/main/java/algorithm/study/pureun/programmers/Prg_120888.java
@@ -1,5 +1,7 @@
 package algorithm.study.pureun.programmers;
 
+import java.util.LinkedList;
+
 /**
  * 프로그래머스 - 중복된 문자 제거
  *
@@ -16,10 +18,29 @@ public class Prg_120888 {
 	
 	public String solution(String my_string) {
         String answer = "";
+        
+        //char 형태의 리스트
+        char[] ch = my_string.toCharArray();
+        LinkedList<Character> list = new LinkedList<Character>();
+        
+        //리스트의 순서대로 저장
+        for(int i = 0; i <ch.length; i++) {
+        	list.add(ch[i]);
+        }
+        
+        for(int i = list.size(); i > 0; i--) {
+        	for(int j = list.size(); j > 0 ; j-- ) {
+        		if(list.get(i).equals(list.get(j))) {
+        		list.remove(list.get(j));	
+        		}
+        	}
+        }
+        
+        
 
       
         
-        return sn;
+        return answer;
     }
 
 }
